0303c856927e376c8d4051b721ea0b7aea07465d
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-09-12 16:48:39 +0200

    Added comments at the top with answers to the questions

--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -11,7 +11,33 @@
              * 
              * Det var tydligt lite för länge sedan jag höll på med programmering för att lösa det.
              * Jag får se till att sitta ordentligt med videomaterialet så minnet kommer tillbaka.. :P
-             *     
+             *   
+             *   
+             * Jag skriver svaren på frågorna här:  
+             *   
+                    Fråga 1 - Vilka klasser
+
+                        Class1: employeeID, employeeName
+                        Class2: salaryID, employeeSalary
+
+                    Fråga 2 - Vilka attribut och metoder 
+
+                        Class1:
+                            här behövs:
+		                    1st string-attribut: den anställdes namn
+		                    1st INT: den anställdes ID (på så sätt kan man kolla det mot lönens ID så att rätt lön kan kopplas till rätt person)
+
+                        Class2:
+                        	Här behövs:
+                    		1 decimal-attribut för själva lönen
+		                    1st INT: lönensID
+
+                    Varje gång en ny post läggs till får personen och lönen samma ID (ex. 42).
+                    När man sen listar de anställda kan man lista namnen med visst ID tillsammans med lön med samma ID.
+
+             *   
+             *   Jag har för mig det går att skapa EN lista med flera olika data-typer dock... Så det är nog en såndan man ska använda här. Men just nu står det stilla i mitt huvud..
+             *   
              */
 
 
